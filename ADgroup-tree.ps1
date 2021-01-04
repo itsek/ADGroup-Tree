dcb9e255397a02b4addc08ef299a25ba2ad9cf8f
e-its <76947368+e-its@users.noreply.github.com>
2021-01-04 12:43:22 +0100

    Add files via upload

---
+++ ADgroup-tree.ps1
@@ -0,0 +1,25 @@
+﻿[cmdletbinding()]
+param (
+    [parameter(ValueFromPipeline)]
+    $Groupname = "",
+    $Depth = " "
+)
+    
+$Members = Get-ADObject -Identity $Groupname -Properties Member | Select-Object -ExpandProperty Member | Sort-Object -Property @{expr={(Get-ADObject -Identity $_).ObjectClass}; desc=$true}
+$Depth = $Depth.Insert(0,"     ")
+Write-Host "$Depth|    Members of $($Groupname)" -ForegroundColor Green
+
+Foreach ($Member in $Members) {
+
+    $Object = Get-ADObject -Identity $Member 
+
+    if ($Object.ObjectClass -eq "user") {
+        Write-Host "$Depth|--- User $($Object.Name)"
+    }
+
+    elseif ($Object.ObjectClass -eq "group") {
+        Write-Host "$Depth|--- Group $($Object.Name)" 
+        Get-GroupMembersRecursively -Groupname $Object.DistinguishedName -Depth $Depth
+        
+    }
+}
